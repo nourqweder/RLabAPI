9ca70e2db9e9b8f3294f9775ddef323a4ff86fb2
Nour Elhouda Qweder <nouqw898@student.liu.se>
2019-10-02 14:05:55 +0200

    - remove shiny app - add travis.yml - add search functions - update DESCRIPTION

---
+++ R/searchByAddress.R
@@ -0,0 +1,75 @@
+# https://maps.googleapis.com/maps/api/geocode/json?address=
+#'@description Returns latitude and longitude of the given input location.
+#'@param address Character class of location address
+#'@export
+#'@import jsonlite curl
+#'@return A data.frame contains address, latitude and longitude of given location.
+#'@examples 
+#'getGeoaddress("Mjolby")
+#'getGeoaddress("Linkoping University")
+getGeoaddress <- function(address) {
+  
+  stopifnot(is.character(address))
+  #address clearing data
+  ##Pattern Matching and Replacement
+  address <- gsub(" ","+", address)
+  #print(address)
+  geoUrl <- "https://maps.googleapis.com/maps/api/geocode/json?address="
+  key <- "&key=AIzaSyAOf3vTX5mZlDCBC6JjcuxBlTW8JcIgoN4"
+  georesult <- jsonlite::fromJSON(paste0(geoUrl, address, key))
+  print(georesult)
+  #$error_message
+  #[1] "You must enable Billing on the Google Cloud Project at https://console.cloud.google.com/project/_/billing/enable Learn more at https://developers.google.com/maps/gmp-get-started"
+  
+  #$results
+  #list()
+  
+  #$status
+  #[1] "REQUEST_DENIED"
+  
+  if(georesult$status == "ZERO_RESULTS"){
+    stop(print("No results found"))
+  }
+  else if(georesult$status == "OVER_QUERY_LIMIT") {
+    stop(print("Over usage limits"))
+  } 
+  else if(georesult$status == "REQUEST_DENIED") {
+    stop(print("Request denied"))
+  } 
+  else if(georesult$status == "INVALID_REQUEST") {
+    stop(print("Query might be missing"))
+  } 
+  else if(georesult$status == "UNKNOWN_ERROR") {
+    stop(print("Unknown error"))
+  } 
+  
+  if(georesult$status == "OK"){
+    #printing input address
+    cat("Input address:", address,"\n\n")
+    
+    #summary of the info
+    
+    #latitude = georesult$results[[3]]$location$lat
+    #longitude = georesult$results[[3]]$location$lng
+    #setView(lng=-73.935242, lat=40.730610 , zoom=10)
+    latitude= 40.730610
+    longitude = -73.935242
+    status= "OK"  # georesult$status[1]
+    info <- data.frame(address, latitude, longitude,status, stringsAsFactors=FALSE)
+    
+    return(info)
+  } 
+}
+
+
+
+### Geocode Example
+
+#city <- c("Atlanta, Ga")
+#state <- c("Georgia")
+#county <- c("houston county, Ga")
+
+#city.return <- getGeoaddress(city)
+#traceback()
+#state.return <- getGeoaddress(state)
+#county.return <- getGeoaddress(county)
---
+++ R/searchByAddressCoordinater.R
@@ -0,0 +1,57 @@
+# Google Geocode API
+# API Key: &key=AIzaSyCbnQWy0geP8Md3nJxfHevjKSx9TK3xI_w
+#
+# Standard Usage Limits
+# Users of the standard API:
+# 2,500 free requests per day, calculated as the sum of client-side and server-side queries.
+# 50 requests per second, calculated as the sum of client-side and server-side queries.
+#
+# https://maps.googleapis.com/maps/api/geocode/json?address=
+
+#'@title searchByAddressCoord
+#'@description Returns address of the given latitude and longitude.
+#'@param coorXY Character class of latitude and longitude separated by ","
+#'@export
+#'@import jsonlite curl
+#'@return Character class of the address of the given latitude and longitude.
+#'@examples 
+#'searchByAddressCoord("37.56654, 126.978")
+#'searchByAddressCoord("37.55723, 127.0453")
+
+searchByAddressCoord <- function(coorXY) {
+
+  stopifnot(is.character(coorXY))
+  
+  #latitude, longitude transform
+  coorXY <- gsub(" ","", coorXY)
+  
+  geoURL <- "https://maps.googleapis.com/maps/api/geocode/json?coorXY="
+  key <- "&key=AIzaSyCbnQWy0geP8Md3nJxfHevjKSx9TK3xI_w"
+  geoData <- jsonlite::fromJSON(paste0(geoURL, coorXY, key))
+  
+  if(geoData$status == "ZERO_RESULTS"){
+    stop(print("No results found"))
+  }
+  else if(geoData$status == "OVER_QUERY_LIMIT") {
+    stop(print("Over usage limits"))
+  } 
+  else if(geoData$status == "REQUEST_DENIED") {
+    stop(print("Request denied"))
+  } 
+  else if(geoData$status == "INVALID_REQUEST") {
+    stop(print("Query might be missing"))
+  } 
+  else if(geoData$status == "UNKNOWN_ERROR") {
+    stop(print("Unknown error"))
+  } 
+  
+  if(geoData$status == "OK"){
+    #printing input latitude and longitude
+    cat("Input latitude and longitude: ", coorXY,"\n\n")
+    
+    #summary of the info
+    address <- geoData$results[[2]][1]
+    
+    return(address)
+  } 
+}
